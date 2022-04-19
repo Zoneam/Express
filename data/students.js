98cd7d2c9c6cb39a04d129e0c4202f3499295abc
Zoneam <zoneam@gmail.com>
2022-04-18 18:40:57 -0700

    solution with bootstrap

--- data/students.js
+++ data/students.js
@@ -1,7 +1,14 @@
 const students = [
-    {student: 'Brian Smith', done: true},
-    {todo: 'Learn Express', done: false},
-    {todo: 'Buy Milk', done: false}
+    {student: 'Brian Smith', age: 25, present: true, grade: 80},
+    {student: 'Lela Huff', age: 32, present: false, grade: 82},
+    {student: 'BTeri Gibson', age: 31, present: true, grade: 84},
+    {student: 'LSon Waller', age: 47, present: true, grade: 40},
+    {student: 'Amanda Golden', age: 58, present: true, grade: 75},
+    {student: 'Lanny Pratt', age: 21, present: false, grade: 76},
+    {student: 'Donn Duarte', age: 26, present: false, grade: 87},
+    {student: 'Tobias Greene', age: 38, present: true, grade: 91},
+    {student: 'Quinn Howard', age: 54, present: true, grade: 100},
+    {student: 'Essie Shields', age: 24, present: false, grade: 74},
   ];
   
   module.exports = {
